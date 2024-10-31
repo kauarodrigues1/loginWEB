6f42aaf88d6f346c2e001ef29ea0e69d4ad1e433
Wellington <welltecnic@hotmail.com>
2024-10-31 11:44:13 -0300

    criando o javascript parte1

---
+++ src/js/script.js
@@ -0,0 +1,69 @@
+//DECLARANDO AS VARIAVEIS
+const form  =document.getElementById("form")
+const nome  =document.getElementById("nome")
+const email =document.getElementById("email")
+const senha =document.getElementById("senha")
+
+//EVENTO QUE VAI PEGAR O QUE ACONTECER NO FORMULARIO
+
+form.addEventListener('submit',(e)=>{
+    //PREVINE QUALQUER ALTERAÇÃO NO FORMULÁRIO
+    e.preventDefault()
+
+    //CHAMAR A FUNÇÃO QUE VAI CHECAR OS CAMPOS
+    checarCampos()
+})
+
+//FUNÇÃO CHECAR CAMPOS
+function checarCampos(){
+    //DECLARANDO AS VARIAVEIS
+    // O METODO TRIM REMOVE ESPAÇOS EM BRANCO NO INICIO E NO FIM DO TEXTO
+    const nomeValor = nome.value.trim()
+    const emailValor = email.value.trim()
+    const senhaValor = senha.value.trim()
+
+    //SE O CAMPO NOME ESTIVER VAZIO
+    if(nomeValor ===""){
+       validarErro(nome,"Preencha o campo nome")
+    }
+    else{
+        validarSucesso(nome)
+    }
+
+    //SE O CAMPO EMAIL ESTIVER VAZIO
+
+    if(emailValor === ""){
+        validarErro(email, "Preencha o campo email")
+    }
+    else{   
+        validarSucesso(email)
+    }
+
+    //SE O CAMPO SENHA ESTIVER VAZIO
+
+    if(senhaValor === ""){
+        validarErro(senha,"Preencha o campo senha")
+    }
+    else if(senhaValor.length < 8)
+    {
+        validarErro(senha,"A senha deve ter mais que 8 caracteres")
+    }else{
+        validarSucesso(senha)
+    }
+
+} 
+
+//VALIDAR ERROR
+
+    function validarErro(input,message){    
+        //RETORNA PARA O ELEMENTO PAI
+        const controle = input.parentElement;
+        // PEGA O PRIMEIRO ELEMENTO SMALL QUE O SELETOR ENCONTRAR
+        const small = controle.querySelector('small')
+        //PEGA A MENSAGEM NO CAMPO EM CASO DE ERRO
+        small.innerText = message
+        //PEGA AS CONFIGURAÇÕES DE CSS DA CLASSE CONTROLE E ERROR
+        controle.className ="controle error"
+    }
+
+//VALIDAR SUCESSO
