2081336a1d190be638b8b47a92b69ab93a867c6d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-07 10:36:24 -0300

    Finalizando a validação do formulario

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
 //DECLARANDO AS VARIAVEIS
-const form  =document.getElementById("form")
-const nome  =document.getElementById("nome")
-const email =document.getElementById("email")
-const senha =document.getElementById("senha")
+const form = document.getElementById("form")
+const nome = document.getElementById("nome")
+const email = document.getElementById("email")
+const senha = document.getElementById("senha")
 
 //EVENTO QUE VAI PEGAR O QUE ACONTECER NO FORMULARIO
 
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit', (e) => {
     //PREVINE QUALQUER ALTERAÇÃO NO FORMULÁRIO
     e.preventDefault()
 
@@ -15,7 +15,7 @@ form.addEventListener('submit',(e)=>{
 })
 
 //FUNÇÃO CHECAR CAMPOS
-function checarCampos(){
+function checarCampos() {
     //DECLARANDO AS VARIAVEIS
     // O METODO TRIM REMOVE ESPAÇOS EM BRANCO NO INICIO E NO FIM DO TEXTO
     const nomeValor = nome.value.trim()
@@ -23,47 +23,53 @@ function checarCampos(){
     const senhaValor = senha.value.trim()
 
     //SE O CAMPO NOME ESTIVER VAZIO
-    if(nomeValor ===""){
-       validarErro(nome,"Preencha o campo nome")
+    if (nomeValor === "") {
+        validarErro(nome, "Preencha o campo nome")
     }
-    else{
+    else {
         validarSucesso(nome)
     }
 
     //SE O CAMPO EMAIL ESTIVER VAZIO
 
-    if(emailValor === ""){
+    if (emailValor === "") {
         validarErro(email, "Preencha o campo email")
     }
-    else{   
+    else {
         validarSucesso(email)
     }
 
     //SE O CAMPO SENHA ESTIVER VAZIO
 
-    if(senhaValor === ""){
-        validarErro(senha,"Preencha o campo senha")
+    if (senhaValor === "") {
+        validarErro(senha, "Preencha o campo senha")
     }
-    else if(senhaValor.length < 8)
-    {
-        validarErro(senha,"A senha deve ter mais que 8 caracteres")
-    }else{
+    else if (senhaValor.length < 8) {
+        validarErro(senha, "A senha deve ter mais que 8 caracteres")
+    } else {
         validarSucesso(senha)
     }
 
-} 
+}
 
 //VALIDAR ERROR
 
-    function validarErro(input,message){    
-        //RETORNA PARA O ELEMENTO PAI
-        const controle = input.parentElement;
-        // PEGA O PRIMEIRO ELEMENTO SMALL QUE O SELETOR ENCONTRAR
-        const small = controle.querySelector('small')
-        //PEGA A MENSAGEM NO CAMPO EM CASO DE ERRO
-        small.innerText = message
-        //PEGA AS CONFIGURAÇÕES DE CSS DA CLASSE CONTROLE E ERROR
-        controle.className ="controle error"
-    }
+function validarErro(input, message) {
+    //RETORNA PARA O ELEMENTO PAI
+    const controle = input.parentElement;
+    // PEGA O PRIMEIRO ELEMENTO SMALL QUE O SELETOR ENCONTRAR
+    const small = controle.querySelector('small')
+    //PEGA A MENSAGEM NO CAMPO EM CASO DE ERRO
+    small.innerText = message
+    //PEGA AS CONFIGURAÇÕES DE CSS DA CLASSE CONTROLE E ERROR
+    controle.className = "controle error"
+}
 
 //VALIDAR SUCESSO
+
+function validarSucesso(input) {
+    //RETORNA PARA O ELEMENTO PAI
+    const controle = input.parentElement
+    //PEGA A CLASSE DO CONTROLE DE SUCESSO
+    console.className = 'controle sucesso'
+}
